34e9eb555fdcfc8a9cf5eaaff4e32a7fbc65565b
Michael07220823 <michael31703@gmail.com>
2021-01-08 19:49:10 +0800

    Move position.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="mongo",
-    version="0.0.3",
+    version="0.0.4",
     author="Overcomer",
     author_email="michael31703@gmail.com",
     description="Mongodb operation.",
@@ -13,10 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Michael07220823/mongo.git",
     keywords="mongo",
-    python_requires='>=3',
+    python_requires='~=3.6',
     install_requires=['RandomWordGenerator', 'pypandoc', 'pymongo', 'new_timer'],
     license="MIT",
-    packages=setuptools.find_packages(include=["mongo.py"]),
+    packages=setuptools.find_packages(include=["mongo"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
